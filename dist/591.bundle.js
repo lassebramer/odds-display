c091b4b24ec95f76749aa21a6a9ef1a00e505c20
Lasse Bramer Schmidt <lassebramer@gmail.com>
2024-09-22 17:38:55 +0200

    first build

---
+++ dist/591.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkodds_display_app=self.webpackChunkodds_display_app||[]).push([[591],{591:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
